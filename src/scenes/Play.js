0fc46e370c99eb7a262fddc73b95b6fa786df0cc
revlin <irhoads@ucsc.edu>
2022-04-23 02:47:12 -0700

    colrororrr

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,9 +13,9 @@ class Play extends Phaser.Scene {
     create(){
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);        
         this.add.rectangle(0,borderUISize+borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0); //green bar
-        this.add.rectangle(0,0,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0,0,game.config.width,borderUISize,0xFF00FF).setOrigin(0,0);
         this.borderleft = this.add.tilesprite(0,game.config.height - borderUISize,game.config.width,borderUISize, 'borderleft').setOrigin(0,0); //left
-        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0,0,borderUISize, game.config.height,0xFF00FF).setOrigin(0,0);
         this.borderright = this.add.tilesprite(game.config.width - borderUISize, 0, borderUISize, game.config.height, 'borderright').setOrigin(0,0);//right
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
