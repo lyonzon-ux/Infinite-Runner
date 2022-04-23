5b10c3cf9de168741dc38fc69c7be9f04e64adea
revlin <irhoads@ucsc.edu>
2022-04-23 01:34:01 -0700

    BORDER TEST

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,7 +15,7 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0,0,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,game.config.height - borderUISize,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFCFF).setOrigin(0,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0x00000).setOrigin(0,0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
