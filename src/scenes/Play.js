91a49af5dccc90a178640b56d7c62a12f483b212
revlin <irhoads@ucsc.edu>
2022-04-23 01:46:49 -0700

    color test

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -13,8 +13,8 @@ class Play extends Phaser.Scene {
     create(){
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);        this.add.rectangle(0,borderUISize+borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
         this.add.rectangle(0,0,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(0,game.config.height - borderUISize,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(0,game.config.height - borderUISize,game.config.width,borderUISize,0xFF0000).setOrigin(0,0);
+        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0); //right
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFF0000).setOrigin(0,0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
