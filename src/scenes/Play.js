09495313b5174682876479f945d679bf2c303f52
revlin <irhoads@ucsc.edu>
2022-04-23 02:26:19 -0700

    i hate this

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -14,8 +14,8 @@ class Play extends Phaser.Scene {
         this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);        this.add.rectangle(0,borderUISize+borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
         this.add.rectangle(0,0,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,game.config.height - borderUISize,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
-        this.add.rectangle(0,0,borderUISize, game.config.height,0xFF0000).setOrigin(0,0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFF0000).setOrigin(0,0);//right
+        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0);
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0,0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
