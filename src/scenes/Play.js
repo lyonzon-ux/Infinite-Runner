99725e2c0860daa2cd02a0f7bb0ab36a37c2d75e
revlin <irhoads@ucsc.edu>
2022-04-23 02:37:05 -0700

    kill meeeeee

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -15,7 +15,7 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0,borderUISize+borderPadding, game.config.width, borderUISize * 2, 0x00FF00).setOrigin(0,0);
         this.add.rectangle(0,0,game.config.width,borderUISize,0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(0,game.config.height - borderUISize,game.config.width,borderUISize,0xFF00FF).setOrigin(0,0);
-        this.add.rectangle(0,0,borderUISize, game.config.height,0xFF0000).setOrigin(0,0);
+        this.add.rectangle(0,0,borderUISize, game.config.height,0xFFFFFF).setOrigin(0,0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFF00FF).setOrigin(0,0); //right
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
