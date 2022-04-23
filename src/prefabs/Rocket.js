e46b7134698eda8f175ef249425f0ff97c48f94c
revlin <irhoads@ucsc.edu>
2022-04-22 23:45:13 -0700

    please

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -17,9 +17,10 @@ class Rocket extends Phaser.GameObjects.Sprite{
                 this.x += this.moveSpeed;
             }
         }
-        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring){
+        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
-            this.sfxRocket.play();
+            this.sfxRocket.play();  // play sfx
+          
         }
         if(this.isFiring && this.y >= borderUISize*3+borderPadding){
             this.y -=this.moveSpeed;
