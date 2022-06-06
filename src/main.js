0d09bb1c39ab206ef0d8546ba18297a306d839b5
Lou Yonzon <Marytalens@gmail.com>
2022-06-06 04:17:24 -0700

    Headers

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+//Loumary Yonzon + Ian Rhoads 
+//Completed 6 June 2022
+//A twist on Rocket Patrol, but the timer and scores are affected by which asset the player collides into 
+//Assets have been changed but due to complications, the SFX have not been changed 
+//Playable game based off the playtests ran
+
+
+
 let config = {
     type: Phaser.AUTO,
     width: 480,
@@ -9,4 +17,4 @@ let keyF, keyR, keyLEFT, keyRIGHT, mousePointer;
 // set UI size
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
-//commenting to help 
+ 
